75bdd98152770c3e3d7f925a9438d0b913508060
ilarikoik <ilari.koik@gmail.com>
2025-06-02 13:34:06 +0300

    settingsscreenstuff

--- frontend/app/navigations/loginNavigation.tsx
+++ frontend/app/navigations/loginNavigation.tsx
@@ -1,18 +1,18 @@
-import "react-native-gesture-handler";
+// import "react-native-gesture-handler";
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "../screens/home";
-import Login from "../screens/login";
-import CreateAccount from "../screens/createAccout";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import Home from "../screens/home";
+// import Login from "../screens/login";
+// import CreateAccount from "../screens/createAccout";
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
 
-export default function LoginNavigation() {
-  return (
-    <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Create" component={CreateAccount} />
-    </Stack.Navigator>
-  );
-}
+// export default function LoginNavigation() {
+//   return (
+//     <Stack.Navigator initialRouteName="Login">
+//       <Stack.Screen name="Home" component={Home} />
+//       <Stack.Screen name="Create" component={CreateAccount} />
+//     </Stack.Navigator>
+//   );
+// }
--- frontend/app/navigations/tabs/settings.tsx
+++ frontend/app/navigations/tabs/settings.tsx
@@ -1,6 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import ItemList from "../../components/itemList";
 import LogoutButton from "../../components/logout";
@@ -8,14 +15,40 @@ import { useUser } from "../../context/userContext";
 
 export default function Settings() {
   const { user } = useUser();
-  const [data, setData] = useState(Array.from({ length: 3 }, (_, i) => i + 1));
 
   return (
     <>
-      <LogoutButton />
-      <ItemList data={data} itemHeight={40} />
+      <View style={styles.con}>
+        <View style={styles.settingscon}>
+          <Text style={styles.text}>{user ? "Theme toggle Text" : "Dark"}</Text>
+          <Text style={styles.text}>You have x upcoming events.</Text>
+          <Text style={styles.text}>You had x events.</Text>
+          <LogoutButton />
+        </View>
+      </View>
     </>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  con: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  settingscon: {
+    height: "60%",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: "#fff",
+  },
+  text: {
+    fontSize: 22,
+    textAlign: "center",
+    borderBottomColor: "#ccc",
+    margin: 10,
+  },
+});
