49e3fc4e3583b91d71cf1ef6c61bb94830caac83
ilarikoik <ilari.koik@gmail.com>
2025-06-04 13:36:12 +0300

    contacts searchbar + addcontact modal

---
+++ frontend/app/components/addContactModal.tsx
@@ -0,0 +1,188 @@
+import React, { useState } from "react";
+import {
+  Alert,
+  Modal,
+  StyleSheet,
+  Text,
+  Pressable,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { useTheme } from "../context/themeContext";
+
+type ModalProps = {
+  toggleModal: () => void;
+  modalVisible: boolean;
+};
+
+export const ContactModal = ({ toggleModal, modalVisible }: ModalProps) => {
+  const { theme, colors } = useTheme();
+  const handleModal = () => {
+    toggleModal();
+  };
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={modalVisible}
+      style={styles.centeredView}
+      //   onRequestClose={() => {
+      //     Alert.alert("Modal has been closed.");
+      //     setModalVisible(!modalVisible);
+      //   }}
+    >
+      <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+          <View style={styles.row}>
+            <Text style={{ textAlign: "left", width: "100%" }}>Etunimi:</Text>
+            <TextInput
+              style={[
+                styles.input,
+                {
+                  backgroundColor: theme === "dark" ? colors.text : "#ccc",
+                  color: theme === "dark" ? colors.background : "#000",
+                },
+              ]}
+              placeholderTextColor="#999"
+              returnKeyType="done"
+            />
+            <Text style={{ textAlign: "left", width: "100%" }}>Sukunimi:</Text>
+            <TextInput
+              style={[
+                styles.input,
+                {
+                  backgroundColor: theme === "dark" ? colors.text : "#ccc",
+                  color: theme === "dark" ? colors.background : "#000",
+                },
+              ]}
+              placeholderTextColor="#999"
+              returnKeyType="done"
+            />
+            <Text style={{ textAlign: "left", width: "100%" }}>
+              Sähköposti:
+            </Text>
+            <TextInput
+              style={[
+                styles.input,
+                {
+                  backgroundColor: theme === "dark" ? colors.text : "#ccc",
+                  color: theme === "dark" ? colors.background : "#000",
+                },
+              ]}
+              placeholderTextColor="#999"
+              returnKeyType="done"
+            />
+            <Text style={{ textAlign: "left", width: "100%" }}>
+              Puhelinnumero:
+            </Text>
+            <TextInput
+              style={[
+                styles.input,
+                {
+                  backgroundColor: theme === "dark" ? colors.text : "#ccc",
+                  color: theme === "dark" ? colors.background : "#000",
+                },
+              ]}
+              placeholderTextColor="#999"
+              returnKeyType="done"
+            />
+          </View>
+          <View style={styles.buttoncon}>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={handleModal}
+            >
+              <Text style={styles.textStyle}>Close</Text>
+            </Pressable>
+            <Pressable
+              style={[styles.button, styles.buttonSave]}
+              onPress={handleModal}
+            >
+              <Text style={styles.textStyle}>Save</Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.7)",
+  },
+  modalView: {
+    height: "60%",
+    width: "90%",
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttoncon: {
+    flexDirection: "row",
+    width: 300,
+    height: 50,
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "49%",
+    height: 50,
+    borderRadius: 10,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    // backgroundColor: "#F194FF",
+    // backgroundColor: "blue",
+  },
+  buttonClose: {
+    backgroundColor: "#fe4833",
+  },
+  buttonSave: {
+    backgroundColor: "#1ed760",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+  row: {
+    height: "70%",
+    width: "100%",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  input: {
+    margin: 10,
+    height: 50,
+    padding: 4,
+    borderRadius: 6,
+    width: "100%",
+  },
+});
+
+export default ContactModal;
--- frontend/app/navigations/tabs/contacts.tsx
+++ frontend/app/navigations/tabs/contacts.tsx
@@ -1,18 +1,58 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import ItemList from "../../components/itemList";
 import { useTheme } from "../../context/themeContext";
+import ContactModal from "../../components/addContactModal";
 
 export default function Contacts() {
   const [data, setData] = useState(Array.from({ length: 50 }, (_, i) => i + 1));
+  const [modalVisible, setModalVisible] = useState(false);
+  const toggleModal = () => setModalVisible(!modalVisible);
   const { theme, setTheme, colors } = useTheme();
 
+  const handleAdd = () => {
+    setModalVisible(!modalVisible);
+  };
   return (
     <>
       <View style={[styles.con, { backgroundColor: colors.background }]}>
+        {/* <SafeAreaProvider>
+          <SafeAreaView
+            style={[styles.con, { backgroundColor: colors.background }]}
+          > */}
+        <View style={styles.row}>
+          <TextInput
+            style={[
+              styles.input,
+              {
+                backgroundColor: theme === "dark" ? colors.text : "#ccc",
+                color: theme === "dark" ? colors.background : "#000",
+              },
+            ]}
+            placeholder="Search"
+            placeholderTextColor="#999"
+            returnKeyType="done"
+          />
+
+          <TouchableOpacity style={styles.addButton} onPress={handleAdd}>
+            <Text style={[styles.addText, { color: "#fff" }]}>Add</Text>
+          </TouchableOpacity>
+        </View>
+        <ContactModal toggleModal={toggleModal} modalVisible={modalVisible} />
+
         <ItemList data={data} itemHeight={50}></ItemList>
+        {/* </SafeAreaView>
+        </SafeAreaProvider> */}
       </View>
     </>
   );
@@ -21,5 +61,35 @@ export default function Contacts() {
 const styles = StyleSheet.create({
   con: {
     flex: 1,
+    justifyContent: "center",
+  },
+  buttoncon: {
+    // backgroundColor: "red",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    marginRight: 10,
+  },
+  addButton: {
+    backgroundColor: "#2196F3",
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    borderRadius: 8,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  addText: {
+    fontWeight: "bold",
   },
 });
